12078a288389ad4c5b38757f6e63e4aa5aa95d27
Dan Blomquist <dblomqu1@gmail.com>
2026-09-06 11:52:42 -0400

    Add GA4 bootstrap for 906 dashboard (G-MP8ERM5MQC)

---
+++ sites/906/ga4.js
@@ -0,0 +1,14 @@
+(function () {
+  if (window.__dashGa4Booted) return;
+  window.__dashGa4Booted = true;
+  var MEASUREMENT_ID = 'G-MP8ERM5MQC';
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', MEASUREMENT_ID);
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=' + MEASUREMENT_ID;
+  document.head.appendChild(s);
+})();
